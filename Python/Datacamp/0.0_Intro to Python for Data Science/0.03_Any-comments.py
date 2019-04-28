4ce35723415f7af8c0efbaf5179b5e6de1b6a594
100523629 <20.ommarkhan@gmail.com>
2019-04-27 17:09:21 -0700

    Reuploading old files

---
+++ Python/Datacamp/0.0_Intro to Python for Data Science/0.03_Any-comments.py
@@ -0,0 +1,15 @@
+# 0.03
+# Any comments?
+#Something that Filip didn't mention in his videos is that you can add comments to your Python scripts. Comments are important to make sure that you and others can understand what your code is about.
+
+# To add comments to your Python script, you can use the # tag. These comments are not run as Python code, so they will not influence your result. As an example, take the comment on the right, # Division; it is completely ignored during execution.
+
+# INSTRUCTIONS
+100 XP
+Above the print(7 + 10), add the comment # Addition.
+
+# Just testing division
+print(5 / 8)
+
+# Addition works too
+print(7 + 10)
